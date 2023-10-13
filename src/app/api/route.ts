78ee5222ac9f69f496553b576e4326f59b71c03f
Satyam Bansal <satyam@passageprotocol.com>
2023-10-13 17:51:24 +0530

    Change nft metadata

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,31 +3,10 @@ import { NextRequest } from 'next/server'
 export async function GET(req: NextRequest) {
   const data = {
     name: 'SillyApe #',
-    description: 'a ERC2114 test run for starkpills',
+    description: 'a ERC721 test run for sillyape',
     image:
       'https://arweave.net/3tsgIKE0d0F-56pDXRpm2E7a-_c-IfUJB7wyhwDhr9o/TestPill/pill_012000.png',
-    attributes: [
-      {
-        trait_type: 'Medical Bill',
-        value: 3000000000000000,
-      },
-      {
-        trait_type: 'Ingredient',
-        value: 'Cartridge',
-      },
-      {
-        trait_type: 'Background',
-        value: 'White',
-      },
-      {
-        trait_type: 'Fame',
-        value: 0,
-      },
-      {
-        trait_type: 'DeFame',
-        value: 0,
-      },
-    ],
+    attributes: [],
   }
   const id = req.nextUrl.searchParams.get('id')
   const payload = data
